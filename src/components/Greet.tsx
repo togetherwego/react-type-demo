5a70f123f35a82c9266f9ca6dd703d6a358c889c
firyalhuwaida <iyaalfiryal@gmail.com>
2022-05-14 16:58:20 +0700

    add: props

---
+++ src/components/Greet.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+type GreetProps = {
+  name: string;
+  messageCount: number;
+  isLoggedIn: boolean;
+};
+
+export const Greet = (props: GreetProps) => {
+  return (
+    <div>
+      <h1>
+        {props.isLoggedIn
+          ? `Hallo ${props.name}, You have ${props.messageCount} unread message`
+          : " Welcome Guest"}
+      </h1>
+    </div>
+  );
+};
