eadfc3f3cccfbbc37ae062c6fe0e062f9569568b
firyalhuwaida <iyaalfiryal@gmail.com>
2022-05-17 07:41:22 +0700

    style props

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Heading } from "./components/Heading";
 import { Oscar } from "./components/Oscar";
 import { Button } from "./components/Button";
 import { Input } from "./components/Input";
+import { Container } from "./components/Container";
 
 function App() {
   const personName = {
@@ -38,10 +39,13 @@ function App() {
       <Oscar>
         <Heading>Oscar goes to surabaya</Heading>
       </Oscar>
-      <Button handleClick={(event, id) => {
-        console.log('Button clicked', event, id);
-      }}></Button>
-      <Input value='' handleChange={event => console.log(event)}/>
+      <Button
+        handleClick={(event, id) => {
+          console.log("Button clicked", event, id);
+        }}
+      ></Button>
+      <Input value="" handleChange={(event) => console.log(event)} />
+      <Container styles={{ border: "1px solid black", padding: "1rem" }} />
     </div>
   );
 }
